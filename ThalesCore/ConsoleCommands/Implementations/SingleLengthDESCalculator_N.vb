1c9b709a05d6880f34bf07ea294229c6ffb4d488
nickntg_cp <SND\nickntg_cp>
2009-12-07 19:41:52 +0000

    Version 0.8.5.

---
+++ ThalesCore/ConsoleCommands/Implementations/SingleLengthDESCalculator_N.vb
@@ -0,0 +1,72 @@
+﻿''
+'' This program is free software; you can redistribute it and/or modify
+'' it under the terms of the GNU General Public License as published by
+'' the Free Software Foundation; either version 2 of the License, or
+'' (at your option) any later version.
+''
+'' This program is distributed in the hope that it will be useful,
+'' but WITHOUT ANY WARRANTY; without even the implied warranty of
+'' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+'' GNU General Public License for more details.
+''
+'' You should have received a copy of the GNU General Public License
+'' along with this program; if not, write to the Free Software
+'' Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+'' 
+
+Imports ThalesSim.Core
+Imports ThalesSim.Core.Resources
+Imports ThalesSim.Core.Cryptography
+
+Namespace ConsoleCommands
+
+    ''' <summary>
+    ''' Single-length DES calculator.
+    ''' </summary>
+    ''' <remarks></remarks>
+    <ThalesConsoleCommandCode("N", "Encrypts data using a single-length DES key.")> _
+    Public Class SingleLengthDESCalculator_N
+        Inherits AConsoleCommand
+
+        ''' <summary>
+        ''' Stack initialization.
+        ''' </summary>
+        ''' <remarks></remarks>
+        Public Overrides Sub InitializeStack()
+            _stack.PushToStack(New ConsoleMessage("Enter data: ", "", New Validators.HexKeyValidator))
+            _stack.PushToStack(New ConsoleMessage("Enter key: ", "", New Validators.HexKeyValidator))
+        End Sub
+
+        ''' <summary>
+        ''' Encrypts and returns the result
+        ''' </summary>
+        ''' <returns></returns>
+        ''' <remarks></remarks>
+        Public Overrides Function ProcessMessage() As String
+
+            Dim data As String = _inStack.PopFromStack.ConsoleMessage
+            Dim desKey As String = _inStack.PopFromStack.ConsoleMessage
+
+            If desKey.Length <> 16 Then
+                Return "INVALID KEY"
+            End If
+
+            If data.Length <> 16 Then
+                Return "INVALID DATA"
+            End If
+
+            If Utility.IsParityOK(desKey, Utility.ParityCheck.OddParity) = False Then
+                Return "KEY PARITY ERROR"
+            End If
+
+            Dim hk As New HexKey(desKey)
+            Dim crypt As String = TripleDES.TripleDESEncrypt(hk, data)
+            Dim decrypt As String = TripleDES.TripleDESDecrypt(hk, data)
+
+            Return "Encrypted: " + MakeKeyPresentable(crypt) + vbCrLf + _
+                   "Decrypted: " + MakeKeyPresentable(decrypt)
+        End Function
+
+    End Class
+
+End Namespace
---
+++ ThalesCore/ConsoleCommands/Validators/DataLengthValidator.vb
@@ -0,0 +1,42 @@
+﻿''
+'' This program is free software; you can redistribute it and/or modify
+'' it under the terms of the GNU General Public License as published by
+'' the Free Software Foundation; either version 2 of the License, or
+'' (at your option) any later version.
+''
+'' This program is distributed in the hope that it will be useful,
+'' but WITHOUT ANY WARRANTY; without even the implied warranty of
+'' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+'' GNU General Public License for more details.
+''
+'' You should have received a copy of the GNU General Public License
+'' along with this program; if not, write to the Free Software
+'' Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+'' 
+
+Namespace ConsoleCommands.Validators
+
+    ''' <summary>
+    ''' This validator verifies that a user enters S, D or T.
+    ''' </summary>
+    ''' <remarks></remarks>
+    Public Class DataLengthValidator
+        Implements IConsoleDataValidator
+
+        ''' <summary>
+        ''' Determines whether the user enters S, D or T.
+        ''' </summary>
+        ''' <param name="consoleMsg">Console message to validate.</param>
+        ''' <remarks>If the enter key is not valid, the validator 
+        ''' throws a <see cref="Exceptions.XInvalidData"/> exception.</remarks>
+        Public Sub ValidateConsoleMessage(ByVal consoleMsg As String) Implements IConsoleDataValidator.ValidateConsoleMessage
+            'Slight modification to allow the use of lower-case characters.
+            consoleMsg = consoleMsg.ToUpper
+
+            If consoleMsg <> "S" AndAlso consoleMsg <> "D" AndAlso consoleMsg <> "T" Then
+                Throw New Exceptions.XInvalidData("INVALID LENGTH")
+            End If
+        End Sub
+    End Class
+
+End Namespace
