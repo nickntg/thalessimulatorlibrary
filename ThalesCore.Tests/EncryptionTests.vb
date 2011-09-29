9412f5f629627f567ad11127417cb9c07c0507a4
Nick Bitounis <nickntg@hotmail.com>
2011-09-29 22:31:49 +0000

    Added an implementation of MS command thanks to Cory.

--- ThalesCore.Tests/EncryptionTests.vb
+++ ThalesCore.Tests/EncryptionTests.vb
@@ -19,6 +19,8 @@ Imports System.Text
 Imports System.Collections.Generic
 Imports Microsoft.VisualStudio.TestTools.UnitTesting
 Imports ThalesSim.Core.Cryptography
+Imports ThalesSim.Core.Cryptography.MAC
+Imports ThalesSim.Core
 
 <TestClass()> Public Class EncryptionTests
 
@@ -61,5 +63,34 @@ Imports ThalesSim.Core.Cryptography
 
         Dim sResult As String = TripleDES.TripleDESEncryptVariant(New HexKey(LMK.LMKStorage.LMKVariant(Core.LMKPairs.LMKPair.Pair28_29, 2)), "F1F1F1F1F1F1F1F1C1C1C1C1C1C1C1C1")
         Assert.AreEqual(sResult, "5178C9D3D1052B15BF6AEC458B4A4564")
+    End Sub
+
+    <TestMethod()> _
+    Public Sub TestX919Mac()
+        Dim hk As New HexKey("838652DF68A246046DAB6104583B201A")
+        Dim strdata As String = "30303030303030303131313131313131"
+        Dim IV As String = "0000000000000000"
+
+        'Test binary data MACing.
+        Dim hexString As String = String.Empty
+        Utility.ByteArrayToHexString(Utility.GetBytesFromString("00000000"), hexString)
+        Assert.AreEqual("3F431586CA33D99C", ISOX919MAC.MacHexData(hexString, hk, IV, ISOX919Blocks.OnlyBlock))
+
+        'Test binary un-padded data MACing.
+        Utility.ByteArrayToHexString(Utility.GetBytesFromString("000000001"), hexString)
+        Assert.AreEqual("2FE7118F2074398E", ISOX919MAC.MacHexData(hexString, hk, IV, ISOX919Blocks.OnlyBlock))
+
+        'Test successive hex data MACing.
+        IV = ISOX919MAC.MacHexData(strdata, hk, IV, ISOX919Blocks.FirstBlock)
+        Assert.AreEqual("A9D4D96683B51333", IV)
+        IV = ISOX919MAC.MacHexData(strdata, hk, IV, ISOX919Blocks.NextBlock)
+        Assert.AreEqual("DA46CEC9E61AF065", IV)
+        IV = ISOX919MAC.MacHexData(strdata, hk, IV, ISOX919Blocks.NextBlock)
+        Assert.AreEqual("56A27E35442BD07D", IV)
+        IV = ISOX919MAC.MacHexData(strdata, hk, IV, ISOX919Blocks.NextBlock)
+        Assert.AreEqual("B12874BED7137303", IV)
+        IV = ISOX919MAC.MacHexData(strdata, hk, IV, ISOX919Blocks.FinalBlock)
+        Assert.AreEqual("0D99127F7734AA58", IV)
+
     End Sub
 End Class
