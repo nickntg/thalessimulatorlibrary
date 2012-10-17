5ed87921ad1f6d16c71d9ad989b27771f10c5f92
Nick Bitounis <nickntg@hotmail.com>
2012-10-17 20:18:42 +0000

    C# migration.
    * LMK storage.
    * Parity checks.

--- ThalesSim.Core/Cryptography/LMK/LmkStorage.cs
+++ ThalesSim.Core/Cryptography/LMK/LmkStorage.cs
@@ -16,6 +16,8 @@
 
 using System.Collections.Generic;
 using System.IO;
+using System.Runtime.InteropServices;
+using System.Text;
 using ThalesSim.Core.Utility;
 
 namespace ThalesSim.Core.Cryptography.LMK
@@ -64,28 +66,152 @@ namespace ThalesSim.Core.Cryptography.LMK
             _oldLmKs = ReadLmkFile(LmkOldStorageFile);
         }
 
-        public static void GenerateTestLmks()
+        public static void GenerateTestLmks(bool writeLmks = true)
         {
-            
+            if (string.IsNullOrEmpty(LmkStorageFile))
+            {
+                throw new InvalidOleVariantTypeException("No storage file specified");
+            }
+
+            const string staticLmks =
+                @"; LMK Storage file
+                01010101010101017902CD1FD36EF8BA
+                20202020202020203131313131313131
+                40404040404040405151515151515151
+                61616161616161617070707070707070
+                80808080808080809191919191919191
+                A1A1A1A1A1A1A1A1B0B0B0B0B0B0B0B0
+                C1C1010101010101D0D0010101010101
+                E0E0010101010101F1F1010101010101
+                1C587F1C13924FEF0101010101010101
+                01010101010101010101010101010101
+                02020202020202020404040404040404
+                07070707070707071010101010101010
+                13131313131313131515151515151515
+                16161616161616161919191919191919
+                1A1A1A1A1A1A1A1A1C1C1C1C1C1C1C1C
+                23232323232323232525252525252525
+                26262626262626262929292929292929
+                2A2A2A2A2A2A2A2A2C2C2C2C2C2C2C2C
+                2F2F2F2F2F2F2F2F3131313131313131
+                01010101010101010101010101010101";
+            using (var ms = new MemoryStream(staticLmks.GetBytes()))
+            {
+                _lmKs = ReadLmkFile(ms);
+            }
+
+            const string staticOldLmks =
+                @"; LMK Storage file
+                101010101010101F7902CD1FD36EF8BA
+                202020202020202F3131313131313131
+                404040404040404F5151515151515151
+                616161616161616E7070707070707070
+                808080808080808F9191919191919191
+                A1A1A1A1A1A1A1AEB0B0B0B0B0B0B0B0
+                C1C101010101010ED0D0010101010101
+                E0E001010101010EF1F1010101010101
+                1C587F1C13924FFE0101010101010101
+                010101010101010E0101010101010101
+                020202020202020E0404040404040404
+                070707070707070E1010101010101010
+                131313131313131F1515151515151515
+                161616161616161F1919191919191919
+                1A1A1A1A1A1A1A1F1C1C1C1C1C1C1C1C
+                232323232323232F2525252525252525
+                262626262626262F2929292929292929
+                2A2A2A2A2A2A2A2F2C2C2C2C2C2C2C2C
+                2F2F2F2F2F2F2FFE3131313131313131
+                010101010101010E0101010101010101";
+
+            using (var ms = new MemoryStream(staticOldLmks.GetBytes()))
+            {
+                _oldLmKs = ReadLmkFile(ms);
+            }
+
+            if (writeLmks)
+            {
+                WriteLmks();
+            }
+        }
+
+        public static void WriteLmks()
+        {
+            WriteLmks(LmkStorageFile, _lmKs);
+            WriteLmks(LmkOldStorageFile, _oldLmKs);
+        }
+
+        public static string DumpLmks()
+        {
+            var sb = new StringBuilder();
+            for (var pair = LmkPair.Pair00_01; pair <= LmkPair.Pair34_35; pair++)
+            {
+                sb.AppendLine(Lmk(pair));
+            }
+
+            return sb.ToString();
+        }
+
+        public static string GenerateLmkCheckValue()
+        {
+            var text = Lmk(LmkPair.Pair00_01).XorHex(Lmk(LmkPair.Pair02_03));
+            for (var pair = LmkPair.Pair04_05; pair <= LmkPair.Pair34_35; pair++)
+            {
+                text = text.XorHex(Lmk(pair));
+            }
+
+            return text.Substring(0, 16).XorHex(text.Substring(16));
+        }
+
+        public static bool CheckLmkStorage()
+        {
+            for (var pair = LmkPair.Pair00_01; pair <= LmkPair.Pair34_35; pair++)
+            {
+                if (!Lmk(pair).IsParityOk(Parity.Odd))
+                {
+                    return false;
+                }
+            }
+
+            return true;
         }
 
         private static SortedList<LmkPair, string> ReadLmkFile (string storageFile)
         {
+            using (var sr = new StreamReader(storageFile, Encoding.Default))
+            {
+                return ReadLmkFile(sr.BaseStream);
+            }
+        }
+
+        private static SortedList<LmkPair, string> ReadLmkFile (Stream stream)
+        {
             var lst = new SortedList<LmkPair, string>();
             var index = LmkPair.Pair00_01;
-            using (var sr = new StreamReader(storageFile, System.Text.Encoding.Default))
+            using (var sr = new StreamReader(stream))
             {
                 while (!sr.EndOfStream)
                 {
                     var line = sr.ReadLine();
                     if (!string.IsNullOrEmpty(line) && !line.StartsWith(";"))
                     {
-                        lst.Add(index, line);
+                        lst.Add(index, line.Trim());
                         index++;
                     }
                 }
             }
             return lst;
+        }
+
+        private static void WriteLmks(string storageFile, SortedList<LmkPair, string> lst)
+        {
+            using (var sw = new StreamWriter(storageFile, false, Encoding.Default))
+            {
+                sw.WriteLine("; LMK Storage File");
+                for (var pair = LmkPair.Pair00_01; pair <= LmkPair.Pair34_35; pair++)
+                {
+                    sw.WriteLine(lst[pair]);
+                }
+            }
         }
     }
 }
