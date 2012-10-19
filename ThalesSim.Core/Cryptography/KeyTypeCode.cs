6384cf3578520a54579647343d0449d28bc6b958
nickntg_cp <SND\nickntg_cp>
2012-10-19 16:45:33 +0000

    C# migration
    * PIN block & PIN block format.
    * Key types.

---
+++ ThalesSim.Core/Cryptography/KeyTypeCode.cs
@@ -0,0 +1,46 @@
+﻿/*
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+using System;
+using ThalesSim.Core.Cryptography.LMK;
+using ThalesSim.Core.Utility;
+
+namespace ThalesSim.Core.Cryptography
+{
+    public class KeyTypeCode
+    {
+        public LmkPair Pair { get; set; }
+
+        public int Variant { get; set; }
+
+        public KeyTypeCode (string keyTypeCode)
+        {
+            if (string.IsNullOrEmpty(keyTypeCode) || keyTypeCode.Length != 3 || !keyTypeCode.IsHex())
+            {
+                throw new InvalidCastException(string.Format("Invalid key type code {0}", keyTypeCode));
+            }
+
+            if (!char.IsDigit(keyTypeCode.ToCharArray()[0]))
+            {
+                throw new InvalidCastException(string.Format("Invalid variant number {0}", keyTypeCode.Substring(0,1)));
+            }
+
+            Variant = Convert.ToInt32(keyTypeCode.Substring(0, 1));
+
+            Pair = keyTypeCode.Substring(1).GetLmkPair();
+        }
+    }
+}
---
+++ ThalesSim.Tests.Unit/Cryptography/PIN/PinBlockTests.cs
@@ -0,0 +1,54 @@
+﻿/*
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+using NUnit.Framework;
+using ThalesSim.Core.Cryptography;
+using ThalesSim.Core.Cryptography.PIN;
+
+namespace ThalesSim.Tests.Unit.Cryptography.PIN
+{
+    [TestFixture]
+    public class PinBlockTests
+    {
+        [Test]
+        public void TestPinBlockCreation()
+        {
+            var pb1 = new PinBlock("1234", "550000025321", PinBlockFormat.AnsiX98);
+        }
+
+        [Test]
+        public void AnsiX98PinBlock()
+        {
+            var pb1 = new PinBlock("1234", "550000025321", PinBlockFormat.AnsiX98);
+            Assert.AreEqual("041261FFFFFDACDE", pb1.ClearPinBlock);
+
+            var pb2 = new PinBlock("CAE9C83F58DDC12D", "550000025321", PinBlockFormat.AnsiX98,
+                                   new HexKey("0123456789ABCDEFABCDEF0123456789"));
+            Assert.AreEqual("1234", pb2.Pin);
+        }
+
+        [Test]
+        public void DieboldPinBlock()
+        {
+            var pb1 = new PinBlock("1234", "550000025321", PinBlockFormat.Diebold);
+            Assert.AreEqual("1234FFFFFFFFFFFF", pb1.ClearPinBlock);
+
+            var pb2 = new PinBlock("4DBA042DD998BE4D", "550000025321", PinBlockFormat.Diebold,
+                                   new HexKey("0123456789ABCDEFABCDEF0123456789"));
+            Assert.AreEqual("1234", pb2.Pin);            
+        }
+    }
+}
